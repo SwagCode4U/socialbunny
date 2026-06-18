9c0c514ff6954c377d937e46f5064e58513269ab
Code With Amit! <swagfor1@proton.me>
2026-06-18 08:06:01 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
 import './index.css'
 
-const GOOGLE_CLIENT_ID = '644247388235-u8p8vdmdvftdgaf8o2qjqjrqnqb693ur.apps.googleusercontent.com'
+const GOOGLE_CLIENT_ID = '64424744e44388235-u8p8vdmdvftdgffffgaf8o2qjqjrqnqb693ur.apps.googleusercontent.com'   //This kind You Keep or better keep in .env
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
